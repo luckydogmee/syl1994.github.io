fa3fc2377972843b4b69468b8d062c029df95977
syll <917154699@qq.com>
2017-03-30 18:26:01 +0800

    修改了一些细节

--- js/demo.js
+++ js/demo.js
@@ -18,18 +18,17 @@ $(document).ready(function(){
 	
 	//当点击博客和随笔的时候阻止默认事件并弹出待开发弹出框
 	$('.blog,.suibi').click(function(e){
-		$('.confirm').removeClass('hide');
-		$(document).click(function(){
-			if(!$('.confirm').hasClass('hide')){
-				$('.confirm').addClass('hide');
-				
+		$('.alert').removeClass('hide');
+		/*$(document).click(function(){
+			if(!$('.alert').hasClass('hide')){
+				$('.alert').addClass('hide');
 			}
-		})
+		})*/
 		//阻止事件冒泡，这个很重要
 		e.stopPropagation();
 	})
-	$('.confirm .btn').click(function(){
-		$('.confirm').addClass('hide');
+	$('.alert .btn').click(function(){
+		$('.alert').addClass('hide');
 	})
 	
 	//导航栏效果
@@ -150,63 +149,5 @@ $(document).ready(function(){
 			$(this).next().show('fast',showMore)
 		})
 	}
-	jianjin('.some-text h3')
-	
-//	//绘制圆形进度条
-//	setTimeout(timer('html5',0,90,50),700);
-//	
-//	
-//	/*
-//	 * 执行定时任务
-//	 * id：画布id
-//	 * start：初始百分比
-//	 * end：结束百分比
-//	 * interval：时间间隔
-//	 */
-//	function timer(id,start,end,interval){
-//	    window.setTimeout(function(){
-//	        circle(id,start/100);
-//	        start++;
-//	        if(start<end+1){
-//	            timer(id,start,end,interval);
-//	        }
-//	    },interval);
-//	}
-//	function circle(id,percent){
-//	    var p=(percent*100).toFixed(0);
-//	    var c=document.getElementById(id);
-//	    var cxt=c.getContext("2d");
-//	    //生成圆形（底圆）
-//	    cxt.fillStyle="#D0D0D0";
-//	    cxt.beginPath();
-//	    cxt.moveTo(75, 75); 
-//	    cxt.arc(75,75,70,0,Math.PI*2,false);
-//	    cxt.closePath();
-//	    cxt.fill();
-//	    //生成扇形
-//	    cxt.fillStyle="#0268BD";
-//	    cxt.beginPath();
-//	    cxt.moveTo(75, 75);
-//	    if(percent==1){
-//	        cxt.arc(75,75,70,0,Math.PI*2,false);
-//	    }else if(percent==0){
-//	        cxt.arc(75,75,70,0,0,true);
-//	    }else{
-//	        cxt.arc(75,75,70,Math.PI,Math.PI+Math.PI*2*percent,false);
-//	    }
-//	    cxt.closePath();
-//	    cxt.fill();
-//	    //生成圆形（上层园）
-//	    cxt.fillStyle="#FFFFFF";
-//	    cxt.beginPath();
-//	    cxt.moveTo(75, 75); 
-//	    cxt.arc(75,75,50,0,Math.PI*2,false);
-//	    cxt.closePath();
-//	    cxt.fill();
-//	    //生成中间百分比文字
-//	    cxt.font="20px arial";
-//	    cxt.fillStyle="#000000";
-//	    cxt.fillText(p+"%",55,75);
-//	//  cxt.fillText("0%",160,75);
-//	}		
+	jianjin('.some-text h3')	
 })
